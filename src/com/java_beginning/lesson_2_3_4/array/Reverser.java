0acf673d952d91b2efe4d5a9f90ee52bbfd37b40
javaprogrammer45 <natureandpeople@gmail.com>
2024-12-16 17:11:31 +0300

    +

--- src/com/java_beginning/lesson_2_3_4/array/Reverser.java
+++ src/com/java_beginning/lesson_2_3_4/array/Reverser.java
@@ -5,33 +5,39 @@ import java.util.Arrays;
 public class Reverser {
     public static void main(String[] args) {
         Reverser reverser = new Reverser();
-        int[] nullArray = null;
         int[] zeroArray = new int[0];
+        int[] nullArray = null;
         int[] oneArray = {6, 8, 9, 1};
         int[] twoArray = {13, 8, 5, 3, 2, 1, 1};
 
-        reverser.outputtingArrayValues(zeroArray);
-        reverser.outputtingArrayValues(nullArray);
-        reverser.outputtingArrayValues(oneArray);
-        reverser.outputtingArrayValues(twoArray);
+        reverser.printResult(zeroArray, reverser.reverse(zeroArray));
+        reverser.printResult(nullArray, reverser.reverse(nullArray));
+        reverser.printResult(oneArray, reverser.reverse(oneArray));
+        reverser.printResult(twoArray, reverser.reverse(twoArray));
+
     }
 
-    public void outputtingArrayValues(int[] arr) {
-        if (arr == null) {
-            System.out.println("null");
-        } else if (arr.length == 0) {
-            System.out.println("массив нулевой длины");
+    public void printResult(int[] arr1, int[] arr2) {
+        if (arr1 == null || arr2 == null) {
+            return;
+        } else if (arr1.length == 0 || arr2.length == 0) {
+            return;
         } else {
-            System.out.print("До реверса: " + Arrays.toString(arr) + "\nПосле реверса: ");
-            reverse(arr);
+            System.out.print("\nДо реверса: " + Arrays.toString(arr1) + "\nПосле реверса: " + Arrays.toString(arr2) + "\n");
         }
     }
 
-    private void reverse(int[] values) {
-        System.out.print("[");
-        for (int i = 0; i < values.length - 1; i++) {
-            System.out.print(values[values.length - 1 - i] + ", ");
+    private int[] reverse(int[] values) {
+        if (values == null) {
+            System.out.println("null");
+            return null;
+        } else if (values.length == 0) {
+            System.out.println("массив нулевой длины");
         }
-        System.out.println(values[0] + "]");
+        int[] newValues = new int[values.length];
+        for (int i = 0; i <= values.length - 1; i++) {
+            newValues[i] = values[values.length - 1 - i];
+        }
+        return newValues;
     }
 }
--- src/com/java_beginning/lesson_2_3_4/array/Triangle.java
+++ src/com/java_beginning/lesson_2_3_4/array/Triangle.java
@@ -1,6 +1,81 @@
 package com.java_beginning.lesson_2_3_4.array;
 
 public class Triangle {
+    private final int startChars = 33;
+    private final int endChars = 47;
+    private final int startNumbers = 48;
+    private final int endNumbers = 57;
+    private final int startUppercaseLetters = 65;
+    private final int endUppercaseLetters = 90;
+
+
+    public Triangle() {
+    }
+
+
+    public int getStartChars() {
+        return startChars;
+    }
+
+    public int getEndChars() {
+        return endChars;
+    }
+
+    public int getStartNumbers() {
+        return startNumbers;
+    }
+
+    public int getEndNumbers() {
+        return endNumbers;
+    }
+
+    public int getStartUppercaseLetters() {
+        return startUppercaseLetters;
+    }
+
+    public int getEndUppercaseLetters() {
+        return endUppercaseLetters;
+    }
+
+
     public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.displayTriangle(50,46);
+    }
+
+    public void displayTriangle(int start, int end) {
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean direction = (start < end && end == getEndChars() || end == getEndNumbers() || end == getEndUppercaseLetters());
+        if (direction) {
+            int size = end - start;
+            for (int i = 1; i <= size + 1; i++) {
+                for (int j = 1; j <= size + 1 - i; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print(stringBuilder.repeat(end, i));
+                stringBuilder.delete(0, stringBuilder.length());
+                if (i >= 2) {
+                    for (int j = size; j < size * 2; j++) {
+                        System.out.print(stringBuilder.repeat(end, i));
+                        stringBuilder.delete(0, stringBuilder.length());
+                        break;
+                    }
+                }
+                end--;
+                System.out.println();
+            }
+        } else if (!direction) {
+            if (start > end) {
+                System.out.println("Ошибка: левая граница " + start + " > правой " + end);
+            } else {
+                System.out.println("Введите корректные данные...");
+            }
+
+        }
+
     }
 }
+
+
+
+

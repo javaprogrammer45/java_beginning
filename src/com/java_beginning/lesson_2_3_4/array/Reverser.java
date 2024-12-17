15371101ac0d2d0b61c61bbb5216e00f9078e88a
javaprogrammer45 <natureandpeople@gmail.com>
2024-12-17 17:40:11 +0300

    Corrected

--- src/com/java_beginning/lesson_2_3_4/array/Reverser.java
+++ src/com/java_beginning/lesson_2_3_4/array/Reverser.java
@@ -10,21 +10,10 @@ public class Reverser {
         int[] oneArray = {6, 8, 9, 1};
         int[] twoArray = {13, 8, 5, 3, 2, 1, 1};
 
-        reverser.printResult(zeroArray, reverser.reverse(zeroArray));
-        reverser.printResult(nullArray, reverser.reverse(nullArray));
-        reverser.printResult(oneArray, reverser.reverse(oneArray));
-        reverser.printResult(twoArray, reverser.reverse(twoArray));
-
-    }
-
-    public void printResult(int[] arr1, int[] arr2) {
-        if (arr1 == null || arr2 == null) {
-            return;
-        } else if (arr1.length == 0 || arr2.length == 0) {
-            return;
-        } else {
-            System.out.print("\nДо реверса: " + Arrays.toString(arr1) + "\nПосле реверса: " + Arrays.toString(arr2) + "\n");
-        }
+        reverser.printReversedArrays(zeroArray, reverser.reverse(zeroArray));
+        reverser.printReversedArrays(nullArray, reverser.reverse(nullArray));
+        reverser.printReversedArrays(oneArray, reverser.reverse(oneArray));
+        reverser.printReversedArrays(twoArray, reverser.reverse(twoArray));
     }
 
     private int[] reverse(int[] values) {
@@ -34,10 +23,19 @@ public class Reverser {
         } else if (values.length == 0) {
             System.out.println("массив нулевой длины");
         }
-        int[] newValues = new int[values.length];
-        for (int i = 0; i <= values.length - 1; i++) {
-            newValues[i] = values[values.length - 1 - i];
+        int[] reversedValues = new int[values.length];
+        int index = 0;
+        for (int number : values) {
+            reversedValues[reversedValues.length - 1 - index++] = number;
         }
-        return newValues;
+        return reversedValues;
+    }
+
+    public void printReversedArrays(int[] arr1, int[] arr2) {
+        if (arr1 == null || arr2 == null || arr1.length == 0 || arr2.length == 0) {
+            return;
+        }
+        System.out.print("\nДо реверса: " + Arrays.toString(arr1) +
+                "\nПосле реверса: " + Arrays.toString(arr2) + "\n");
     }
 }

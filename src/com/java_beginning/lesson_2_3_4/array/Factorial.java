25c0980451337a23c70324d6df089b7948405703
javaprogrammer45 <natureandpeople@gmail.com>
2024-12-17 16:37:16 +0300

    +

--- src/com/java_beginning/lesson_2_3_4/array/Factorial.java
+++ src/com/java_beginning/lesson_2_3_4/array/Factorial.java
@@ -1,45 +1,71 @@
 package com.java_beginning.lesson_2_3_4.array;
 
+import java.util.Arrays;
+
 public class Factorial {
     public static void main(String[] args) {
         Factorial factorial = new Factorial();
 
         int[] zeroArray = new int[0];
+        int[] nullArray = null;
         int[] firstArray = {8, 0, 9};
         int[] secondArray = {-3, 1, 7, 13};
         int[] thirdArray = {-22, -0};
 
-        factorial.calculateFactorial(zeroArray);
-        factorial.calculateFactorial(firstArray);
-        factorial.calculateFactorial(secondArray);
-        factorial.calculateFactorial(thirdArray);
+        //factorial.printFactorials(factorial.calculateFactorial(zeroArray));
+        //factorial.printFactorials(factorial.calculateFactorial(nullArray));
+        factorial.printFactorials(factorial.calculateFactorial(firstArray));
+//        factorial.printFactorials(factorial.calculateFactorial(secondArray));
+//        factorial.printFactorials(factorial.calculateFactorial(thirdArray));
+        // factorial.calculateFactorial(firstArray);
     }
 
-    public void calculateFactorial(int... varargs) {
-        int[] newArray;
-        if (varargs.length == 0) {
-            System.out.println("массив нулевой длины");
+    public void printFactorials(int[] array) {
+        if (array == null) {
+            System.out.println("null");
+
+        } else if (array.length == 0) {
             return;
+        } else {
+            int result = 1;
+            System.out.print(array.length + "!= ");
+            for (int i = 0; i < array.length; i++) {
+                result *= array[i];
+                if (i != array.length - 1) {
+                    System.out.print(array[i] + "*");
+                } else {
+                    System.out.println(array[array.length - 1] + " = " + result);
+                }
+            }
+        }
+    }
+
+    public int[] calculateFactorial(int... varargs) {
+        if (varargs == null) {
+            return null;
+        }
+        int[] array = varargs;
+        int[] arr = new int[varargs.length];
+        if (array.length == 0) {
+            System.out.println("массив нулевой длины");
+        } else {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] < 0) {
+                    System.out.println("Ошибка: факториал " + array[i] + "!  не определен");
+                } else if (array[i] == 0 || array[i] == 1) {
+                    System.out.println("Факториал 0 и 1 = 1");
+                }
+                if (array[i] != 0 && array[i] != 1 && array[i] > 0) {
+                    int[] newArray = new int[array[i]];
+                    for (int j = 0; j < array[i]; j++) {
+                        newArray[j] = j + 1;
+                    }
+                    arr = newArray;
+                }
+            }
         }
 
-        for (int i = 0; i < varargs.length; i++) {
-            int result = 1;
-            if (varargs[i] < 0) {
-                System.out.println("Ошибка: факториал " + varargs[i] + "!  не определен");
-                result = varargs[i];
-                System.out.println(result);
-            } else if (varargs[i] == 0 || varargs[i] == 1) {
-                System.out.println(result);
-            } else {
-                System.out.print(varargs[i] + " = ");
-                for (int j = 1; j <= varargs[i]; j++) {
-                    result *= j;
-                    System.out.print((j != varargs[i]) ? j + " * " : "");
-                }
-                System.out.print(varargs[i] + "\n");
-            }
-            varargs[i] = result;
-        }
-        newArray = varargs;
+        return arr;
+
     }
 }
--- src/com/java_beginning/lesson_2_3_4/array/UniqueNumbers.java
+++ src/com/java_beginning/lesson_2_3_4/array/UniqueNumbers.java
@@ -1,2 +1,50 @@
-package com.java_beginning.lesson_2_3_4.array;public class UniqueNumbers {
+package com.java_beginning.lesson_2_3_4.array;
+
+import java.util.Arrays;
+
+public class UniqueNumbers {
+    public static void main(String[] args) {
+
+        UniqueNumbers uniqueNumbers = new UniqueNumbers();
+
+        //uniqueNumbers.sortingArray(uniqueNumbers.fillingArray(-30, -10));
+//        uniqueNumbers.printArray(uniqueNumbers.sortingArray(uniqueNumbers.fillingArray(-30, -10)), 23);
+    }
+
+
+    public void printArray(int[] arr, int lengthString) {
+        int[] arr2 = new int[lengthString];
+        int counter = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            counter++;
+            arr2[i] = arr[i];
+            if (arr.length != arr2.length) {
+                arr2[i] = counter;
+
+            }
+            System.out.print(arr2[i] +" ");
+        }
+    }
+
+    public int[] fillingArray(int start, int end) {
+        int[] array = new int[((end - start) / 4) * 3];
+        for (int i = 0; i <= array.length-1; i++) {
+            int number = (int) (Math.random() * (end - start + 1)) + start;
+            array[i] = number;
+        }
+        return array;
+    }
+
+//    public  sortingArray(int[] arr) {
+//        for (int i = 0; i < arr.length-1; i++) {
+//            if (arr[i] > arr[i + 1]) {
+//                int temp = arr[i];
+//                arr[i] = arr[i+1];
+//                arr[i+1] = temp;
+//            }
+//
+//        }
+//        return arr;
+//    }
+
 }

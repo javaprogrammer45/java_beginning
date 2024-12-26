66d543bb6ddb9c6621ed5304c8cda6db39708642
javaprogrammer45 <natureandpeople@gmail.com>
2024-12-26 19:30:20 +0300

    Corrected

--- src/com/java_beginning/lesson_2_3_4/array/UniqueNumbers.java
+++ src/com/java_beginning/lesson_2_3_4/array/UniqueNumbers.java
@@ -4,53 +4,52 @@ import java.util.Arrays;
 
 public class UniqueNumbers {
     public static void main(String[] args) {
-        UniqueNumbers uniqueNumbers = new UniqueNumbers();
+        UniqueNumbers un = new UniqueNumbers();
 
-        uniqueNumbers.printArray(uniqueNumbers.sortArray(uniqueNumbers.fillingArray(-30, -10, 23)));
-        uniqueNumbers.printArray(uniqueNumbers.sortArray(uniqueNumbers.fillingArray(10, 50, 10)));
-        uniqueNumbers.printArray(uniqueNumbers.sortArray(uniqueNumbers.fillingArray(-34, -34, 0)));
-        uniqueNumbers.printArray(uniqueNumbers.sortArray(uniqueNumbers.fillingArray(-1, 2, -3)));
-        uniqueNumbers.printArray(uniqueNumbers.sortArray(uniqueNumbers.fillingArray(5, -8, 2)));
+        un.printArray(un.sortArray(un.fillArray(-30, -10, 23)));
+        un.printArray(un.sortArray(un.fillArray(10, 50, 10)));
+        un.printArray(un.sortArray(un.fillArray(-34, -34, 0)));
+        un.printArray(un.sortArray(un.fillArray(-1, 2, -3)));
+        un.printArray(un.sortArray(un.fillArray(5, -8, 2)));
     }
 
-    public int[] fillingArray(int start, int end, int lengthString) {
-        if (lengthString <= 0 || lengthString == 0) {
+    private int[] fillArray(int start, int end, int length) {
+        if (length <= 0 || length == 0) {
             System.out.println("Ошибка: количество чисел в строке не может быть меньше 1 (" +
-                    lengthString + ")");
+                    length + ")");
             return null;
         }
         if (start > end) {
             System.out.println("Ошибка: левая граница " + start + " > " + " правой " + end);
             return null;
         }
-        int[] array = new int[((end - start) / 4) * 3];
-        int[] array1 = new int[lengthString];
+        int lenUnsortedArray = (int) ((end - start) * 0.75);
+        int[] unsortedArray = new int[length];
 
         if ((end - start) != 0) {
-            for (int i = 0; i <= array1.length - 1; i++) {
-                if (array.length != ((end - start) / 4) * 3) {
+            for (int i = 0; i < unsortedArray.length; i++) {
+                if (i < lenUnsortedArray) {
                     int number = (int) (Math.random() * (end - start + 1)) + start;
-                    array1[i] = number;
+                    unsortedArray[i] = number;
                 } else {
-                    int number = (int) (Math.random() * (end - start + 1)) + start;
-                    array1[i] = number;
+                    unsortedArray[i] = unsortedArray[i - lenUnsortedArray];
                 }
             }
         } else {
             System.out.println("Ошибка: длина массива должна быть больше 0 (" + (end - start) + ")");
             return null;
         }
-        return array1;
+        return unsortedArray;
     }
 
-    public int[] sortArray(int[] arr) {
-        if (arr != null) {
-            for (int i = 0; i < arr.length - 1; i++) {
-                for (int j = 0; j < arr.length - 1 - i; j++) {
-                    if (arr[j] > arr[j + 1]) {
-                        int temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
+    private int[] sortArray(int[] unsortedArray) {
+        if (unsortedArray != null) {
+            for (int i = 0; i < unsortedArray.length - 1; i++) {
+                for (int j = 0; j < unsortedArray.length - 1 - i; j++) {
+                    if (unsortedArray[j] > unsortedArray[j + 1]) {
+                        int temp = unsortedArray[j];
+                        unsortedArray[j] = unsortedArray[j + 1];
+                        unsortedArray[j + 1] = temp;
                     }
                 }
             }
@@ -58,12 +57,14 @@ public class UniqueNumbers {
             return new int[0];
         }
 
-        return arr;
+        return unsortedArray;
     }
 
-    private void printArray(int[] arr) {
-        if (arr.length != 0) {
-            System.out.println(Arrays.toString(arr) + "\n");
+    private void printArray(int[] sortedArray) {
+        if (sortedArray.length != 0) {
+
+            System.out.println(Arrays.toString(sortedArray) +
+                    "  Колличество чисел в строке = " + sortedArray.length + "\n");
         }
     }
 }

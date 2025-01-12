d014f83919a462f05d2ce482f542c8c6ac3d6bdb
javaprogrammer45 <natureandpeople@gmail.com>
2025-01-12 12:40:21 +0300

    +

--- src/com/java_beginning/lesson_2_3_4/array/ExceedValue.java
+++ src/com/java_beginning/lesson_2_3_4/array/ExceedValue.java
@@ -5,14 +5,10 @@ import java.util.Arrays;
 public class ExceedValue {
     public static void main(String[] args) {
         ExceedValue exceedValue = new ExceedValue();
-        exceedValue.print(exceedValue.fillSourceArray(),
-                exceedValue.changeArray(exceedValue.fillSourceArray(), -1), "Изменённый массив");
-        exceedValue.print(exceedValue.fillSourceArray(),
-                exceedValue.changeArray(exceedValue.fillSourceArray(), 15), "Изменённый массив");
-        exceedValue.print(exceedValue.fillSourceArray(),
-                exceedValue.changeArray(exceedValue.fillSourceArray(), -0), "Изменённый массив");
-        exceedValue.print(exceedValue.fillSourceArray(),
-                exceedValue.changeArray(exceedValue.fillSourceArray(), 14), "Изменённый массив");
+        exceedValue.print(exceedValue.changeArray(-1), "Изменённый массив");
+        exceedValue.print(exceedValue.changeArray(15), "Изменённый массив");
+        exceedValue.print(exceedValue.changeArray(-0), "Изменённый массив");
+        exceedValue.print(exceedValue.changeArray(14), "Изменённый массив");
     }
 
     private float[] fillSourceArray() {
@@ -23,13 +19,14 @@ public class ExceedValue {
         return array;
     }
 
-    private float[] changeArray(float[] sourceArray, int index) {
+    private float[] changeArray(int index) {
+        float[] sourceArray = fillSourceArray();
         if (index < 0) {
-            System.out.println("Отрицательный индекс. Индекс = " + index + "\n\n");
+            System.out.println("Ошибка: индекс не может быть отрицательным (" + index + ")\n\n");
             return null;
         }
         if (index >= sourceArray.length) {
-            System.out.println("Индекс должен быть меньше (15) и больше или равно (0). Индекс = " + index + "\n\n");
+            System.out.println("Ошибка: текущий индекс (" + index + ") должен быть >= 0 и < 15\n\n");
             return null;
         }
         for (int i = 0; i < sourceArray.length; i++) {
@@ -37,19 +34,19 @@ public class ExceedValue {
                 sourceArray[i] = 0;
             }
             if (i == sourceArray.length - 1) {
-                System.out.printf("%s%.3f%n", "значение из ячейки по переданному индексу  ", sourceArray[index]);
+                System.out.printf("%s%d%s%.3f%n", "значение из ячейки по переданному индексу  [", index, "]: ", sourceArray[index]);
             }
         }
         return sourceArray;
     }
 
-    private void print(float[] sourceArray, float[] changedArray, String changeArray) {
+    private void print(float[] changedArray, String changeArray) {
         if (changedArray == null) {
             System.out.println("Массив null\n");
             return;
         }
         for (int i = 0; i < 2; i++) {
-            float[] arrayPrint = Arrays.copyOf(sourceArray, sourceArray.length);
+            float[] arrayPrint = Arrays.copyOf(fillSourceArray(), fillSourceArray().length);
             if (i == 1) {
                 arrayPrint = Arrays.copyOf(changedArray, changedArray.length);
                 System.out.println(changeArray);

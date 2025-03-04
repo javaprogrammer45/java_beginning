4e9048cf3d56c2cf0ed4df16d8d3b43e42dc5171
Andrey <natureandpeople194@gmail.com>
2025-03-04 10:06:13 +0300

    +

--- init_db.sql
+++ init_db.sql
@@ -1,19 +1,16 @@
-
 CREATE DATABASE robots;
 
-CREATE SCHEMA schema;
-
-CREATE TABLE schema.jaegers(
-PRIMARY KEY(jaeger_id),
-jaeger_id  INTEGER  NOT NULL,
-model_name CHAR(20) NOT NULL,
-mark       CHAR(15) NOT NULL,
-height     REAL,
-weight     REAL,  
-status     CHAR(15) NOT NULL,
-origin     CHAR(15),
-launch     INT,
-kaiju_kill INT
+CREATE TABLE jaegers (
+    PRIMARY KEY(jaeger_id),
+    jaeger_id  INTEGER  NOT NULL,
+    model_name CHAR(20) NOT NULL,
+    mark       CHAR(15) NOT NULL,
+    height     REAL,
+    weight     NUMERIC,  
+    status     CHAR(15) NOT NULL,
+    origin     CHAR(15),
+    launch     INT,
+    kaiju_kill INT
 );
 
 \ir 'populate.sql'
